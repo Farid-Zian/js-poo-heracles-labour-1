adbdf616bb298597f789980837c8b0f9c44fcc71
Farid-Zian <130969864+Farid-Zian@users.noreply.github.com>
2023-10-18 12:32:36 +0200

    resolut exercice

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-// First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+const Fighter = require("./src/Fighter.js");
+
+const Heracles = new Fighter("🧔", "20", "6");
+const NemeanLion = new Fighter("🦁", "11", "13");
+
+console.log("🚀 ~ file: index.js:4 ~ Heracles:", Heracles);
+console.log("🚀 ~ file: index.js:6 ~ NemeanLion:", NemeanLion);
+let round = 1;
+while (Heracles.life > 0 && NemeanLion.life > 0) {
+  console.log("round " + round);
+  Heracles.fight(NemeanLion);
+  console.log(
+    `${Heracles.name} attack ${NemeanLion.name}, NemanLion's life: ${NemeanLion.life}`
+  );
+
+  NemeanLion.fight(Heracles);
+  console.log(
+    `${NemeanLion.name} attack ${Heracles.name}, Heracles's life: ${Heracles.life}`
+  );
+  console.log("------------------------------");
+  if (NemeanLion.life === 0) {
+    console.log(` ${Heracles.name} is winner `);
+    console.log(` ${NemeanLion.name} is Dead `);
+    break;
+  }
+  if (Heracles.life === 0) {
+    console.log(` ${NemeanLion.name} is Winner`);
+    console.log(`${Heracles.name} is Dead `);
+    break;
+  }
+  round++;
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,25 @@
-/* Fighter class definition */
+const MAX_LIFE = 100;
+class Fighter {
+  constructor(name, strength, dexterity, life) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+  fight(defender) {
+    let damage = Math.floor(Math.random() * this.strength) + 1;
+
+    if (defender.dexterity > damage) {
+      damage = 0;
+    } else {
+      damage -= defender.dexterity;
+    }
+    if (damage > defender.life) {
+      defender.life = 0;
+    } else {
+      defender.life -= damage;
+    }
+  }
+}
+
+module.exports = Fighter;
